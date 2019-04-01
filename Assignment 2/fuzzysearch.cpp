8fe082e83d284cc210f1061f2f24f081e44ed0ff
timurcarstensen <tm.carstensen@icloud.com>
2019-04-01 13:12:04 +0800

    finished assignment 2

--- Assignment 2/fuzzysearch.cpp
+++ Assignment 2/fuzzysearch.cpp
@@ -1,100 +1,129 @@
-#include <fstream>
+// 20178811 biytc4 Timur Michael Carstensen
 #include <iostream>
-#include <string>
-#include <cstdlib> // for exit()
+#include <fstream>
+#include <sys/stat.h>
 
-int findString(std::string in, std::string text) {
-    using namespace std;
-    int occurrences;
-    string::size_type pos = 0;
-    while ((pos = text.find(in, pos)) != std::string::npos) {
-          ++ occurrences;
-          pos += in.length();
+using namespace std;
+
+class TextFileObject {
+    
+    public: 
+    string data; 
+
+    TextFileObject(string f) { //default constructor
+        // cout << "constructor has been called" << endl;
+        if (checkIfFileIsAccessible(f)) { 
+            fileName = f;                 
+            openFile(fileName);           
+        } else if (!checkIfFileIsAccessible(f)) { 
+            cout << "Cannot open file" << endl;
+            exit (EXIT_FAILURE);
+        }
     }
-    cout << occurrences << " occurences found in this file"<<endl;
+
+    ~TextFileObject() { //default destructor
+        // cout << "destructor has been called" << endl;
+    }; 
+
+    void setInput(string i) {
+        input = i;
+    }
+
+    string findMostSimilarString() {
+        int count = 0;
+        int times = 0;
+        string similarString = "";
+        string tempString = ""; 
+    
+        for (int i = 0; i <= data.length() - input.length(); i++) {
+            int j;
+            times = 0;
+            tempString = "";
+            
+            for (j = 0; j < input.length(); j++) {
+
+                if (data[i+j] == input[j]) {
+                    times++;
+                }
+                    tempString += data[i+j];
+            }
+
+            if (times > count) {
+                count = times;
+                similarString = tempString;
+            }
+        }
+    return similarString;
+    }
+
+    private:
+    string input;
+    string fileName; 
+    string word; 
+
+    void openFile(string f) {
+        fstream file;
+        file.open(f);
+        while (file >> word) {
+
+            data += word + " "; 
+        }
+        file.close();
+    }
+
+    bool checkIfFileIsAccessible(string f) {
+        struct stat buffer;   
+        return (stat (f.c_str(), &buffer) == 0); 
+    }   
+
+};
+
+int findString(string input, string file) {
+    int occurrences;
+    if(input.find_first_not_of(' ') != string::npos) {
+        
+        size_t size = 0;
+
+        while ((size = file.find(input, size)) != string::npos) {
+            ++occurrences;
+            size += input.length();
+        }
+        if (occurrences == 0) {
+            cout << occurrences << " occurences found in this file. ";
+        } else if (occurrences == 1) {
+            cout << occurrences << " occurence found in this file" << endl;
+        } else if (occurrences > 1) {
+            cout << occurrences << " occurences found in this file" << endl;
+        }
+        return occurrences;
+    }
+    return 1;
+}
+
+
+int main(int argc, char *argv[]) {
+   
+    if (argc < 2) {
+      cerr << "Invalid command line arguments. Usage: ./fuzzysearch textfile" << endl;
+      exit(1);
+    }
+
+    TextFileObject obj1(argv[1]); 
+
+    string input = " ";
+    
+    while(!input.empty()) {
+    
+        cout << "Please enter the string to be searched for: ";
+        getline(cin, input);
+
+        int i = findString(input, obj1.data);
+    
+        if (i == 0) {
+            obj1.setInput(input);
+            cout << "Most similar string: " << obj1.findMostSimilarString() << endl;
+        }
+    }
     return 0;
 }
 
- 
-int main(int argc, char* argv[])
-{
-    using namespace std;
-
-    if (argc < 2) 
-    {
-      cerr << "Invalid command line arguments. Usage: ./fuzzysearch textfile" << endl;
-      exit(1);
-    }; 
-
-    ifstream ifs(argv[1]);
-    string content( (istreambuf_iterator<char>(ifs) ),
-                       (istreambuf_iterator<char>()    ) );
-
-    string subString;
-
-    while(true) {
-    cout << "Please enter the string to be searched for: ";
-    string input;
-    getline(cin, input);
-
-    if (!input.empty()) {
-        findString(input, content);
-        subString = input;
-    } else {
-        break;
-    }
-    }
-
-
-    
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     if (argc < 2) 
-//     {
-//       cerr << "Invalid command line arguments. Usage: ./fuzzysearch textfile" << endl;
-//       exit(1);
-//     };  
-
-//     ifstream inf(argv[1]); // ifstream is used for reading files // We'll read from a file called Sample.dat
-
-
-//     if (!inf) // If we couldn't open the input file stream for reading
-//     {
-//         cerr << "Cannot open file" << endl; // Print an error and exit
-//         exit(1);
-//     }
-    
-//     std::string fileText;
-    
-//     while (inf) // While there's still stuff left to read
-//     {
-//         std::string strInput;    
-//         getline(inf, strInput);
-//         cout << strInput << endl;
-//         fileText += strInput;
-//     }
-    
-//     cout << fileText << endl;
-//     // When inf goes out of scope, the ifstream
-//     // destructor will close the file
-// }
